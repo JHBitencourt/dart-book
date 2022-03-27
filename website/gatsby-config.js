743e3f08b43b765b1311bc5d0352fb3320c28f3e
JHBitencourt <julio.henrique.b@gmail.com>
2022-03-27 18:38:57 -0300

    Analytics

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,9 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: `Dart Book`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `O guia do programador de Dart`,
+    siteUrl: `https://www.mydartbook.com.br`,
+    description: `Pegue sua toalha e embarque nessa viagem.`,
+    image: `/green_guy.png`
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-MLR7B9C",
+        includeInDevelopment: false,
+        routeChangeEventName: "gatsby-route-change",
+        enableWebVitalsTracking: true
+      },
+    },
     "gatsby-plugin-styled-components",
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
