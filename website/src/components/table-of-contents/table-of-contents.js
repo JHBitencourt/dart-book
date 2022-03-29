bac80210e9d0767c9af40b199e7a7149fcbbee8d
JHBitencourt <julio.henrique.b@gmail.com>
2022-03-28 23:51:10 -0300

    Short table of contents

--- website/src/components/table-of-contents/table-of-contents.js
+++ website/src/components/table-of-contents/table-of-contents.js
@@ -25,7 +25,15 @@ const TableOfContents = () => {
             <S.ContainerLeftTimeline maxWidth="900px">
               <S.ContentTimeline>
                 <G.Paragraph>
-                  orem aisnfouah iufaiusd gfua sdoug iausdgu adsfg
+                  A primeira parte do livro busca introduzir a história da linguagem e as motivações por trás dela.
+                </G.Paragraph>
+                <G.Paragraph>
+                  Após isso vamos iniciar com o básico da linguagem. Veremos sobre os operadores e estruturas de 
+                  controle existentes. Como funciona o sistema de tipagem escolhido e as mudanças originadas 
+                  pela <i>null safety</i>.
+                </G.Paragraph>
+                <G.Paragraph>
+                  Além de explorar o mundo das funções, métodos, closures e todas essas diversas nomenclaturas.
                 </G.Paragraph>
               </S.ContentTimeline>
             </S.ContainerLeftTimeline>
@@ -33,7 +41,15 @@ const TableOfContents = () => {
             <S.ContainerRightTimeline maxWidth="900px">
               <S.ContentTimeline>
                 <G.Paragraph>
-                  orem aisnfouah iufaiusd gfua sdoug iausdgu adsfg
+                  Na segunda parte, avançamos para outros conceitos fundamentais de programação e como Dart os aborda. 
+                </G.Paragraph>
+                <G.Paragraph>
+                  Veremos sobre tratamento de erros e como criar <i>libraries</i> e <i>packages</i>, junto as suas 
+                  particularidades.
+                </G.Paragraph>
+                <G.Paragraph>
+                  Em seguida abordaremos os conceitos de Orientação a Objetos, finalizando com <i>generics</i> e as estruturas
+                  de dados presentes na linguagem.
                 </G.Paragraph>
               </S.ContentTimeline>
             </S.ContainerRightTimeline>
@@ -41,7 +57,21 @@ const TableOfContents = () => {
             <S.ContainerLeftTimeline maxWidth="900px">
               <S.ContentTimeline>
                 <G.Paragraph>
-                  orem aisnfouah iufaiusd gfua sdoug iausdgu adsfg
+                  Por fim, entraremos nos conceitos mais avançados e também essenciais para o dia a dia do desenvolvedor.
+                </G.Paragraph>
+                <G.Paragraph>
+                  Veremos sobre concorrência e os conceitos de programação assíncrona, além de entender a importância 
+                  do <i>event loop</i> pros trás disso tudo.
+                </G.Paragraph>
+                <G.Paragraph>
+                  Em seguida vamos conhecer o mundo das <i>streams</i> e como elas são úteis em várias situação.
+                </G.Paragraph>
+                <G.Paragraph>
+                  Também nos aprofundaremos no funcionamento das <i>isolates</i> e <i>zones</i>. Dois conceitos pouco
+                  explorados e até conhecidos pelas pessoas que trabalham com a linguagem.
+                </G.Paragraph>
+                <G.Paragraph>
+                  Por fim, com um exemplo prático, aprenderemos a manipular arquivos utilizando todos os conceitos aprendidos.
                 </G.Paragraph>
               </S.ContentTimeline>
             </S.ContainerLeftTimeline>
